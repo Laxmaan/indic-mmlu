a66f2fed87a43b48c7c44d5ac4a46f52519ac380
laxmaanb <laxmaan.balaji@ilink-systems.com>
2024-05-21 14:25:14 -0400

    updated requirements

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 PIP_SRC='./pip_src'
 pip install --src $PIP_SRC -r requirements.txt
 python -c "import nltk; nltk.download('punkt')"
+git clone https://github.com/VarunGumma/IndicTransTokenizer pip_src/IndicTransTokenizer
+pip isntall -e pip_src/IndicTransTokenizer
