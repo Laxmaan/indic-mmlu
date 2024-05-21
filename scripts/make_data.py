47c24ef1018438492723f35093de2fc6d7d0892e
laxmaanb <laxmaan.balaji@ilink-systems.com>
2024-05-21 17:21:22 -0400

    updated make_data

--- scripts/make_data.py
+++ scripts/make_data.py
@@ -5,77 +5,146 @@ import torch
 from transformers import (
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
+    BitsAndBytesConfig
 )
-from IndicTransTokenizer import IndicProcessor
+from IndicTransTokenizer import IndicProcessor, IndicTransTokenizer
 import warnings
+import numpy as np
+from pathlib import Path
 
-def get_preds(input_samples,src_lang='eng_Latn',tgt_lang='hin_Deva',
-              DEVICE='cpu', tokenizer=None,model=None, ip:IndicProcessor=None):
-    with torch.device(DEVICE):
-        batch = ip.preprocess_batch(
-                    input_samples,
-                    src_lang=src_lang,
-                    tgt_lang=tgt_lang,
-                )
-        # Tokenize the sentences and generate input encodings
-        inputs = tokenizer(
-                    batch,
-                    truncation=True,
-                    padding="longest",
-                    return_tensors="pt",
-                    return_attention_mask=True,
-                )
+models = {"small":"ai4bharat/indictrans2-en-indic-dist-200M",
+          "large": "ai4bharat/indictrans2-en-indic-1B"}
 
-        # Generate translations using the model
-        with torch.no_grad():
-                    generated_tokens = model.generate(
-                        **inputs,
-                        use_cache=True,
-                        min_length=0,
-                        max_length=512,
-                        num_beams=5,
-                        num_return_sequences=1,
-                    )
-        # Decode the generated tokens into text
-                
-        with tokenizer.as_target_tokenizer():
-                    generated_tokens = tokenizer.batch_decode(
-                        generated_tokens.detach().cpu().tolist(),
-                        skip_special_tokens=True,
-                        clean_up_tokenization_spaces=True,
-                    )
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+ip = IndicProcessor(inference=True)
+
+
+def initialize_model_and_tokenizer(ckpt_dir, quantization):
+    if quantization == "4-bit":
+        qconfig = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+    elif quantization == "8-bit":
+        qconfig = BitsAndBytesConfig(
+            load_in_8bit=True,
+            bnb_8bit_use_double_quant=True,
+            bnb_8bit_compute_dtype=torch.bfloat16,
+        )
+    else:
+        qconfig = None
+
+    tokenizer = AutoTokenizer.from_pretrained(ckpt_dir, trust_remote_code=True)
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        ckpt_dir,
+        trust_remote_code=True,
+        low_cpu_mem_usage=True,
+        quantization_config=qconfig,
+    )
+
+    if qconfig == None:
+        model = model.to(DEVICE)
+        if DEVICE == "cuda":
+            model.half()
+
+    model.eval()
+
+    return tokenizer, model
+
+
+
+def get_preds(batch,tokenizer,model):
+
+    inputs = tokenizer(
+            batch,
+            truncation=True,
+            padding="longest",
+            return_tensors="pt",
+            return_attention_mask=True,
+        ).to(DEVICE)
+    
+    with torch.no_grad():
+        generated_tokens = model.generate(
+            **inputs,
+            use_cache=True,
+            min_length=0,
+            max_length=512,
+            num_beams=5,
+            num_return_sequences=1,
+        )
+    # Decode the generated tokens into text
+            
+    with tokenizer.as_target_tokenizer():
+        generated_tokens = tokenizer.batch_decode(
+            generated_tokens.detach().cpu().tolist(),
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=True,
+        )
+        
+    return generated_tokens
 
         # Postprocess the translations, including entity replacement
+        
+
+def preprocess_texts(examples,src_lang='eng_Latn',tgt_langs=[],tokenizer=None,model=None):
+    
+    for tgt_lang in tgt_langs:
+        #first preprocess the questions
+        batch = ip.preprocess_batch(
+                examples['question'],
+                src_lang=src_lang,
+                tgt_lang=tgt_lang,
+            )
+        
+        generated_tokens = get_preds(batch,tokenizer,model)
         translations = ip.postprocess_batch(generated_tokens, lang=tgt_lang)
 
-        return translations
+        examples[f'question_{tgt_lang}'] = translations
+        
+         
+        #convert the options.
+        n_options = len(examples['options'][0])
+        flat_options = np.array(examples['options']).flatten()
+        
+        batch = ip.preprocess_batch(
+                flat_options,
+                src_lang=src_lang,
+                tgt_lang=tgt_lang,
+            )
+        
+        generated_tokens = get_preds(batch,tokenizer,model)
+        translations = ip.postprocess_batch(generated_tokens, lang=tgt_lang)
+        
+        translations = np.array(translations).reshape(-1,n_options).tolist()
+        
+        examples[f'options_{tgt_lang}'] = translations
+        
+        return examples
 
 
 
 
-def augment_data(examples):
-    outputs = []
-    for sentence in examples["sentence1"]:
-        words = sentence.split(' ')
-        K = randint(1, len(words)-1)
-        masked_sentence = " ".join(words[:K]  + [mask_token] + words[K+1:])
-        predictions = fillmask(masked_sentence)
-        augmented_sequences = [predictions[i]["sequence"] for i in range(3)]
-        outputs += [sentence] + augmented_sequences
-    return {"data": outputs}
 
-def process_dataset(ds,src_lang='eng_Latn',target_lang='hin_Deva',DEVICE='cpu',model=None,tokenizer=None,ip=None):
-    print(ds)
+def process_dataset(ds,output_dir,tokenizer, model, src_lang='eng_Latn',target_langs=['hin_Deva'],):
+    cols = set(ds.column_names) - {'question','options'}
+    cols = list(cols)
+    translated_ds = ds.map(preprocess_texts, batched=True, fn_kwargs={"src_lang":src_lang, 
+                                                                      'tgt_langs':target_langs,
+                                                                      "tokenizer":tokenizer,
+                                                                      "model":model,
+                                                                      })
+    for tgt_lang in target_langs:
+        lang_ds = translated_ds.select_columns(cols+[f'question_{tgt_lang}',f'options_{tgt_lang}'])
+        lang_ds = lang_ds.rename_columns({
+            f'question_{tgt_lang}':'question',
+            f'options_{tgt_lang}':'options'
+        })
+        
+        lang_ds.save_to_disk(output_dir/f'{tgt_lang}')
     
-    qs = ds['test']['question'][:3]
-    
-    preds = get_preds(qs,src_lang=src_lang, tgt_lang=target_lang,
-              DEVICE=DEVICE,
-              model=model,
-              tokenizer=tokenizer,
-              ip=ip
-              )
-    print(preds)
+   
     
 
 def main(args):
@@ -84,14 +153,19 @@ def main(args):
     print(args)
     print(get_mmlu, get_mmlu_pro)
     
-    model_name = "ai4bharat/indictrans2-en-indic-1B"
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, trust_remote_code=True).to(DEVICE)
-
-    ip = IndicProcessor(inference=True)
+    model_name = models[args.size]
     
+    OUT_DIR = Path(args.output)
+    
+    quantization = f"{args.quantization}-bit" if args.quantization else None
+    
+    tokenizer, model = initialize_model_and_tokenizer(model_name,quantization)
+    process_ds_args = {
+        "src_lang":"eng_Latn",
+        "target_langs":args.target_langs,
+        "tokenizer":tokenizer,
+        "model":model
+    }
     
     datasets_to_process=[]
     if get_mmlu:
@@ -99,15 +173,23 @@ def main(args):
         #rename mmlu features to mmlu_pro features
         ds = ds.rename_columns({"choices":"options",
                                 "subject":"category"})
-        datasets_to_process.append(ds)
+        
+        
+        datasets_to_process.append(process_ds_args | {
+            "ds":ds,
+            "output_dir" : OUT_DIR/ 'mmlu'
+        })
     
     if get_mmlu_pro:
         ds = load_dataset('TIGER-Lab/MMLU-Pro')
-        datasets_to_process.append(ds)
+        datasets_to_process.append(process_ds_args | {
+            "ds":ds,
+            "output_dir" : OUT_DIR/ 'mmlu_pro'
+        })
         
         
     
-    [process_dataset(ds,model=model,DEVICE=DEVICE,tokenizer=tokenizer,ip=ip) for ds in datasets_to_process]
+    [process_dataset(ds) for ds in datasets_to_process]
     
 
 
@@ -115,6 +197,14 @@ if __name__ == "__main__":
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter
     )
+    
+    allowed_langs = [ 'asm_Beng','ben_Beng','brx_Deva','doi_Deva','eng_Latn','gom_Deva','guj_Gujr',
+  'hin_Deva','kan_Knda','kas_Arab','kas_Deva','mai_Deva','mal_Mlym','mar_Deva',
+  'mni_Beng','mni_Mtei','npi_Deva','ory_Orya','pan_Guru','san_Deva','sat_Olck',
+  'snd_Arab','snd_Deva','tam_Taml','tel_Telu','urd_Arab']
+    
+    
+    
     parser.add_argument('-d','--dataset', 
                         choices=['mmlu', 'mmlupro', 'all'], 
                         default='all',
@@ -125,5 +215,25 @@ if __name__ == "__main__":
                         default='./data',
                         help="output folder for the dataset"
                         )
+    parser.add_argument('-t','--target-langs',
+                        choices=allowed_langs, 
+                        nargs='+',
+                        default=['hin_Deva'],
+                        required=True,
+                        help="target languages for conversions"
+                        )
+    parser.add_argument('-s','--size',
+                        choices=['small','large'], 
+                        default='large',
+                        required=False,
+                        help='use the small (distilled 200M) or large (1B) model')
+    parser.add_argument('-q','--quantization',
+                        choices=['4','8',None], 
+                        default=None,
+                        required=False,
+                        help='Quantization to use')
     args = parser.parse_args()
     main(args)
+
+
+
